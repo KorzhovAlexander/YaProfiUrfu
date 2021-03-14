4271f757a502e83c258337b848d246268cd0247a
korzhov.a <honeyiamgood@mail.ru>
2021-03-14 10:08:28 +0300

    swagger

--- YaProfiUrfu/Controllers/NotesController.cs
+++ YaProfiUrfu/Controllers/NotesController.cs
@@ -21,6 +21,22 @@ namespace YaProfiUrfu.Controllers
             _context = context;
         }
 
+        /// <summary>
+        /// Создает заметку
+        /// </summary>
+        /// <remarks>
+        /// В запросе передается body в формате JSON
+        ///  
+        ///     POST /notes
+        ///     {  
+        ///         “title”: “string”,
+        ///         “content”: “string”
+        ///     }
+        /// </remarks>
+        /// <param name="note">Создаваемая запись</param>
+        /// <returns>Новая созданная заметка Note</returns>
+        /// <response code="201">Возвращен если запись была успешно создана</response>
+        /// <response code="400">Если возникла ошибка</response>
         [HttpPost]
         public async Task<ActionResult<Note>> Create(NoteCreateDto note)
         {
@@ -44,13 +60,35 @@ namespace YaProfiUrfu.Controllers
         }
 
 
+        /// <summary>
+        /// Получает все записи
+        /// </summary>
+        /// <returns>Список всех заметок</returns>
+        /// <response code="200">Успешно</response>
         [HttpGet]
         public async Task<IEnumerable<Note>> Get()
         {
             return await _context.Notes.AsNoTracking().ToListAsync();
         }
 
-        [HttpPut("{id}")]
+        /// <summary>
+        /// Изменение заметки
+        /// </summary>
+        /// <remarks>
+        /// В запросе передается body в формате JSON
+        ///  
+        ///     PUT /notes/{id:int}
+        ///     {  
+        ///         “title”: “string”,
+        ///         “content”: “string”
+        ///     }
+        /// </remarks>
+        /// <param name="id">Номер обновляемой заметки</param>
+        /// <param name="note">Новые параметры заметки</param>
+        /// <returns>Обновленная заметка Note</returns>
+        /// <response code="200">Возвращен если запись была успешно обновлена</response>
+        /// <response code="400">Если возникла ошибка</response>
+        [HttpPut("{id:int}")]
         public async Task<ActionResult<Note>> Put(int id, NoteCreateDto note)
         {
             try
@@ -64,6 +102,47 @@ namespace YaProfiUrfu.Controllers
 
                 entity.Content = note.Content;
                 entity.Title = note.Title;
+                await _context.SaveChangesAsync();
+
+                return StatusCode(StatusCodes.Status200OK, entity);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// Удаление заметки
+        /// </summary>
+        /// <remarks>
+        /// В запросе передается body в формате JSON
+        ///  
+        ///     PUT /notes/{id:int}
+        ///     {  
+        ///         “title”: “string”,
+        ///         “content”: “string”
+        ///     }
+        /// </remarks>
+        /// <param name="id">Номер обновляемой заметки</param>
+        /// <param name="note">Новые параметры заметки</param>
+        /// <returns>Обновленная заметка Note</returns>
+        /// <response code="200">Возвращен если запись была успешно обновлена</response>
+        /// <response code="400">Если возникла ошибка</response>
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Note>> Delete(int id)
+        {
+            try
+            {
+                var entity = await _context.Notes.FindAsync(id);
+
+                if (entity == null)
+                {
+                    return NotFound("такой записи нет");
+                }
+
+                _context.Remove(entity);
                 await _context.SaveChangesAsync();
 
                 return StatusCode(StatusCodes.Status200OK, entity);
